11d4011ea25229a7b816fc05aabc421bc3da170a
daffzarco <daffodilzarco@Daffodils-MacBook-Pro.local>
2017-08-01 15:40:43 +1000

    owl carousel
    owl carousel bug fixed

--- js/script.js
+++ js/script.js
@@ -34,9 +34,33 @@ $(document).on('click', '.pulldown-toggle', function(event){
 });
 
   // Return to top
-  $(document).ready(function(){
+$(document).ready(function(){
   $('#return-to-top').click(function() {      // When arrow is clicked
     $('body,html').animate({
         scrollTop : 0                       // Scroll to top of body
     }, 500);
+  });
 });
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    //  loop:true,
+     margin:10,
+     responsiveClass:true,
+     responsive:{
+         0:{
+             items:1,
+             nav:true
+         },
+         600:{
+             items:3,
+             nav:false
+         },
+         1000:{
+             items:5,
+             nav:false
+            //  loop:false
+         }
+     }
+  })
+})
